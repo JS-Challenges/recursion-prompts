c31d6b25eb020018fd7476094f8e93da37a0b3a4
Jose Fernandez <josefernandez@Joses-MacBook-Pro.local>
2021-02-18 10:55:03 -0500

    ASSIGNMENT COMPLETE

--- src/recursion.js
+++ src/recursion.js
@@ -250,7 +250,6 @@ var countValuesInObj = function (obj, value) {
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
 // them to a provided new name while preserving the value stored at that key.
 var replaceKeysInObj = function (obj, oldKey, newKey) {
-  var result = {};
   for (let prop in obj) {
     if (prop === oldKey) {
       obj[newKey] = obj[oldKey];
